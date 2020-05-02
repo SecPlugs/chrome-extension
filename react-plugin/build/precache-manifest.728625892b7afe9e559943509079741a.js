3c5457154ea9fbe0cf05e9885dd2d8a2aca33fa3
Deval-Jain <devaljain@gmail.com>
2020-05-02 19:31:04 +0530

    commit to branch

---
+++ react-plugin/build/precache-manifest.728625892b7afe9e559943509079741a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add437652c59b65bc79120b83ef5075e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "786bab32f7d0f19c4086",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "58ee602fe852f28c783a",
+    "url": "/static/js/2.039019f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.039019f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786bab32f7d0f19c4086",
+    "url": "/static/js/main.373725cb.chunk.js"
+  },
+  {
+    "revision": "b01da1a6bd1fbce08326",
+    "url": "/static/js/runtime-main.cd3f7c43.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
