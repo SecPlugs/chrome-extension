4d80016f923f24afb2782a04012f6b5138ff61e3
Deval-Jain <devaljain@gmail.com>
2020-05-05 17:41:37 +0530

    Use imports and make code modular

--- chrome-plugin/background.js
+++ chrome-plugin/background.js
@@ -1,30 +1,20 @@
+import { fetchGet } from './modules/utils.js'
 chrome.tabs.onUpdated.addListener(function onTabUpdate(tabId, changeInfo, tab) {
-    if(changeInfo.url && !changeInfo.url.includes("chrome")) {                
-        url = "https://api.live.secplugs.com/security/web/quickscan?url=" + changeInfo.url
-        fetch(url, {
-            method: "GET",
-            headers: {
-                "accept": "application/json",
-                "x-api-key": "02Kryf0eidI4c6gNdiUwakibspl8gRm5yTtst69d"
-            }
-        })
-          .then(
-              function(response){
-                  if(response.status === 200) {
-                      response.json().then(
-                          function(data){
-                              if(data["score"] <= 60){
-                                alert("Score for this webpage is " + data["score"])
-                              }else{
-                                  console.log("This webpage is clean")
-                              }  
-                          }
-                      )
-                  }else{
-                      return
-                  }
-              }
-          )
-          .catch()
+    if(changeInfo.url && !changeInfo.url.includes("chrome")) {       
+        let url = "https://api.live.secplugs.com/security/web/quickscan?url=" + changeInfo.url
+        let headers = {
+            "accept": "application/json",
+            "x-api-key": "02Kryf0eidI4c6gNdiUwakibspl8gRm5yTtst69d"
+        }
+        fetchGet(url, headers)
+        .then(data => {
+            if(data["score"] <= 60){
+                alert("Score for this webpage is " + data["score"])
+            }else{
+                console.log("This webpage is clean")
+            } 
+        }
+            
+        )                
     }    
 }); 
