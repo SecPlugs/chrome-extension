3c9d8c300fbc83711a667f16e59376ea2628ebce
Deval-Jain <devaljain@gmail.com>
2020-05-05 22:14:16 +0530

    Set messages as popup on the tab

--- chrome-plugin/background.js
+++ chrome-plugin/background.js
@@ -1,5 +1,6 @@
 import { fetchGet } from './modules/utils.js'
-chrome.tabs.onUpdated.addListener(function onTabUpdate(tabId, changeInfo, tab) {
+let tabArray = []
+chrome.tabs.onUpdated.addListener(function onTabUpdate(tabId, changeInfo, tab) {    
     if(changeInfo.url && !changeInfo.url.includes("chrome")) {       
         let url = "https://api.live.secplugs.com/security/web/quickscan?url=" + changeInfo.url
         let headers = {
@@ -8,13 +9,26 @@ chrome.tabs.onUpdated.addListener(function onTabUpdate(tabId, changeInfo, tab) {
         }
         fetchGet(url, headers)
         .then(data => {
-            if(data["score"] <= 60){
-                alert("Score for this webpage is " + data["score"])
-            }else{
-                console.log("This webpage is clean")
-            } 
-        }
-            
-        )                
-    }    
-}); 
+            if(data["score"] <= 40 && tabArray.indexOf(tabId) === -1){
+                chrome.tabs.executeScript(tabId, 
+                    {code: 'var message = ' + '"Secplug Analysis: This is a malicious page"'},
+                    function(){chrome.tabs.executeScript(tabId, {file: "error_popup.js"})}
+                )
+                tabArray.push(tabId)
+            }else if(data["score"] > 60 && tabArray.indexOf(tabId) === -1){
+                        chrome.tabs.executeScript(tabId, 
+                            {code: 'var message = ' + '"Secplug Analysis: This is a clean page"'},
+                            function(){chrome.tabs.executeScript(tabId, {file: "error_popup.js"})}
+                        )
+                        tabArray.push(tabId)
+            }else if(data["score"] > 40 && data["score"] <= 60 && tabArray.indexOf(tabId) === -1){
+                        chrome.tabs.executeScript(tabId, 
+                            {code: 'var message = ' + '"Secplug Analysis: We do not have threat info of this page"'},
+                            function(){chrome.tabs.executeScript(tabId, {file: "error_popup.js"})}
+                        )
+                        tabArray.push(tabId)
+            }
+        }    
+    )
+    }
+})
---
+++ chrome-plugin/error_popup.js
@@ -0,0 +1,14 @@
+var body   = document.body || document.getElementsByTagName('body')[0],
+    newpar = document.createElement('div');
+
+newpar.style.width = "100%"
+newpar.style.height = "5%"
+newpar.style.zIndex = "100"
+newpar.style.backgroundColor = "coral"
+newpar.style.fontSize = 15
+newpar.style.fontWeight = 500
+newpar.style.textAlign = "center"
+newpar.style.verticalAlign = "middle"
+newpar.style.fontFamily = "sans-serif, Lato, Times New Roman"
+newpar.innerHTML = message;
+body.insertBefore(newpar,body.childNodes[0]);
