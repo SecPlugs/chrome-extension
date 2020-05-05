14a4177bae68b65194257400103e14a88cb4a7c2
Deval-Jain <devaljain@gmail.com>
2020-05-05 17:40:24 +0530

    Remove react plugin

--- react-plugin/build/apps/api_requests.js
+++
@@ -1,6 +0,0 @@
-export const fetchGet = (url, headers_obj) => {
-    const request =  fetch(url, {method: "GET", headers: headers_obj})
-                          .then(response => response.json())
-                          .catch()
-    return request
-}
