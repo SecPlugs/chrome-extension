f10061fd15984a589cc2d908a420abc130b3f226
Deval-Jain <devaljain@gmail.com>
2020-05-07 23:25:29 +0530

    Make it stick

--- chrome-plugin/error_popup.js
+++ chrome-plugin/error_popup.js
@@ -5,11 +5,13 @@ newpar.setAttribute("id", "secplug-error-div")
 newpar.style.width = "100%"
 newpar.style.zIndex = "100"
 newpar.style.backgroundColor = bg_color
-newpar.style.fontSize = 15
-newpar.style.fontWeight = 500
+newpar.style.fontSize = "15"
 newpar.style.textAlign = "center"
 newpar.style.verticalAlign = "middle"
 newpar.style.fontFamily = "sans-serif, Lato, Times New Roman"
+newpar.style.position = "-webkit-sticky"
+newpar.style.position = "sticky"
+newpar.style.top = "0"
 newpar.innerHTML = message;
 
 if(document.getElementById('secplug-error-div')){
