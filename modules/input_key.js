0e8ac1276fa717f22ec12025c1f3e03a801dcf58
Guy <guy@secplugs.com>
2020-12-15 19:35:56 +0000

    api key

--- modules/input_key.js
+++ modules/input_key.js
@@ -41,8 +41,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
 
-
-
             // Scan now
             document.getElementById("scan_link").addEventListener("click", function() {
                 chrome.runtime.sendMessage({ action: "scan_url" }, null);
@@ -61,58 +59,16 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 export const inputKey = () => {
-    var body = document.body || document.getElementsByTagName('body')[0];
-    var newpar = document.createElement('div');
-    var inputBox = document.createElement('input');
-    var buttonKey = document.createElement('input');
 
-    newpar.setAttribute("id", "secplugs-input-div");
-    newpar.style.height = "20%";
-    newpar.style.width = "100%";
-    newpar.style.zIndex = "10000";
-    newpar.style.backgroundColor = "#ffeb99";
-    newpar.style.fontSize = "15";
-    newpar.style.textAlign = "center";
-    newpar.style.verticalAlign = "middle";
-    newpar.style.fontFamily = "sans-serif, Lato, Times New Roman";
-    newpar.style.position = "-webkit-sticky";
-    newpar.style.position = "sticky";
-    newpar.style.bottom = "0";
-    newpar.addEventListener("click", function(event) {
-        if (event.target == event.currentTarget) {
-            utils.closeDiv("secplugs-input-div");
-        }
-    }, false);
+    var api_key_control = document.getElementById("secplugs_popup_api_key_input");
+    var menu_options = document.getElementById("secplugs_popup_menu");
+    var button_key = document.getElementById("secplugs-button-key");
 
-    inputBox.setAttribute("id", "secplugs-input-box");
-    inputBox.setAttribute("type", "text");
-    inputBox.setAttribute("placeholder", "Enter Secplugs API Key");
-    inputBox.style.position = "-webkit-sticky";
-    inputBox.style.position = "sticky";
-    inputBox.style.top = "5%";
-
-    buttonKey.setAttribute("id", "secplugs-button-key");
-    buttonKey.setAttribute("type", "submit");
-    buttonKey.setAttribute("value", "Done");
-    buttonKey.style.border = "None";
-    buttonKey.style.backgroundColor = "black";
-    buttonKey.style.color = "White";
-    buttonKey.style.marginLeft = "5px";
-    buttonKey.style.position = "-webkit-sticky";
-    buttonKey.style.position = "sticky";
-    buttonKey.style.top = "5%";
-    buttonKey.addEventListener("click", function(event) {
+    menu_options.style.visibility = 'hidden';
+    api_key_control.style.visibility = "visible";
+    button_key.addEventListener("click", function(event) {
         if (event.target == event.currentTarget) {
             utils.setKey();
         }
     });
-
-    if (document.getElementById('secplugs-input-div')) {
-        document.getElementById('secplugs-input-div').remove();
-    }
-
-    newpar.appendChild(inputBox);
-    newpar.appendChild(buttonKey);
-    body.insertBefore(newpar, body.childNodes[0]);
-
 };
